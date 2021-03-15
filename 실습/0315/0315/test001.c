c46ed41c0b25d2ed242290341ad473037bfe53a9
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-15 12:48:27 +0900

    \태그

--- 실습/0315/0315/test001.c
+++ 실습/0315/0315/test001.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main(void)
-{
-    printf("Hello C world \n");
-
+int main(void){
+    printf("Hello C world 1 \n");
+    printf("Hello C world 2");
+    printf("Hello C world 3 \n");
     return 0;
 }
