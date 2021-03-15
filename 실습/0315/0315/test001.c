3000d5ddc84331a6532e0bb9ad5d6608c0f7980a
JAEGEUN SONG <76800974+drow724@users.noreply.github.com>
2021-03-15 13:18:20 +0900

    /관련

--- 실습/0315/0315/test001.c
+++ 실습/0315/0315/test001.c
@@ -1,7 +1,10 @@
 #include<stdio.h>
 int main(void){
-    printf("Hello C world 1 \n");
-    printf("Hello C world 2");
-    printf("Hello C world 3 \n");
+    printf("Hello \"C\" world 1 \n");
+    printf("Hello \'C\' world 2\n");
+    printf("Hello\tC world 3 \n");
+    printf("Hello º€¿Á±Ÿ C world 4 \n");
+    printf("Hello abc C world 5 \n");
+    printf("c:\workExample\java \n");
     return 0;
 }
